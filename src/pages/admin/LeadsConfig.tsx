13f14aea9ebbf6e5048745dbb9208eaecb1fe323
Blessed Imobiliaria <contato@blessedimoveis.com.br>
2026-05-28 18:28:49 -0300

    fix: import React explicitly to resolve namespace React TS error

--- src/pages/admin/LeadsConfig.tsx
+++ src/pages/admin/LeadsConfig.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import type { Lead, Property } from "../../types";
 import { formatPropertyId } from "../../utils";
 import { format } from "date-fns";
